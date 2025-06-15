ad655287a856b84fdb2b8b945ef655a111ceed89
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-06-15 21:04:48 +0630

    fix-database.php and login.php

--- config/database.php
+++ config/database.php
@@ -150,11 +150,20 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
+        // 'default' => [
+        //     'url' => env('REDIS_URL'),
+        //     'host' => env('REDIS_HOST', '127.0.0.1'),
+        //     'username' => env('REDIS_USERNAME'),
+        //     'password' => env('REDIS_PASSWORD'),
+        //     'port' => env('REDIS_PORT', '6379'),
+        //     'database' => env('REDIS_DB', '0'),
+        // ],
+
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
+            'host' => env('REDIS_HOST', 'dummy-redis'),
+            'username' => env('REDIS_USERNAME', 'default'),
+            'password' => env('REDIS_PASSWORD', ''),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
--- config/logging.php
+++ config/logging.php
@@ -82,14 +82,28 @@ return [
             'replace_placeholders' => true,
         ],
 
+        // 'papertrail' => [
+        //     'driver' => 'monolog',
+        //     'level' => env('LOG_LEVEL', 'debug'),
+        //     'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
+        //     'handler_with' => [
+        //         'host' => env('PAPERTRAIL_URL'),
+        //         'port' => env('PAPERTRAIL_PORT'),
+        //         'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
+        //     ],
+        //     'processors' => [PsrLogMessageProcessor::class],
+        // ],
+
         'papertrail' => [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'host' => env('PAPERTRAIL_URL', 'logs.dummy.com'),
+                'port' => env('PAPERTRAIL_PORT', 12345),
+                'connectionString' => 'tls://' .
+                    env('PAPERTRAIL_URL', 'logs.dummy.com') . ':' .
+                    env('PAPERTRAIL_PORT', 12345),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
